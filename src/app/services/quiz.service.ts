f086779aca63c7997c5450fdb6f5bdc0f9ec81bb
surekha747 <surejagt@gmail.com>
2021-10-20 18:30:03 +0530

    Init

---
+++ src/app/services/quiz.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class QuizService {
+
+  constructor(private http: HttpClient) { }
+
+  get(url: string) {
+    return this.http.get(url);
+  }
+
+  getAll() {
+    return [
+      { id: '', name: 'Select Quiz' },
+      { id: 'data/java.json', name: 'Java' },
+      { id: 'data/javascript.json', name: 'JavaScript' },
+      { id: 'data/aspnet.json', name: 'Asp.Net' },
+      { id: 'data/csharp.json', name: 'C Sharp' },
+      { id: 'data/designPatterns.json', name: 'Design Patterns' }
+    ];
+  }
+
+}
